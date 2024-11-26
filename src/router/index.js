bd8293ff9bbb6ba7cdf2850dd3fd4ad54abd397e
Siyuan Zheng <z5449047@ad.unsw.edu.au>
2024-11-26 11:37:55 +0800

    Update deployment configuration

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import HomePage from '@/views/HomePage.vue'
 import CourseDetail from '@/views/CourseDetail.vue'
 
@@ -21,7 +21,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory('/unswcourses/'),
   routes
 })
 
